723cb3741e25d2c955d6916884441f84a57f0627
Pradeep.Kumar <Pradeep.Kumar@KCHHQICTL18.KCHUAE.NET>
2022-03-30 20:59:39 +0400

    New changes

---
+++ src/main/java/com/pharmacy/entity/DrugGenerics.java
@@ -0,0 +1,13 @@
+package com.pharmacy.entity;
+
+public class DrugGenerics {
+	
+	private Long drugGenericId;
+
+	private String drugGenericCode;
+	private String drugGenericName;
+	private String numantics;
+	private String quickReferencees;
+	private String mimsRefCode;
+
+}
